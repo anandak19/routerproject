2e74971e7d2863e5f5d444f16c1cdbcbe93e9531
Anandakrishnan H <sree403020@gmail.com>
2024-07-24 21:10:43 +0530

    signup route updated with access date duration

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ import userModel from "../models/user.model.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-// function to register new user
+// register new user
 export const registerUser = async (req, res) => {
   const { email, phoneNumber, userName, password } = req.body;
   try {
@@ -14,18 +14,28 @@ export const registerUser = async (req, res) => {
       return res.status(409).json({ error: "User already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
+    const accessStartDate = new Date();
+    const accessExpiryDate = new Date(accessStartDate);
+    accessExpiryDate.setFullYear(accessExpiryDate.getFullYear() + 1);
 
     const newUser = new userModel({
       email,
       phoneNumber,
       userName,
       password: hashedPassword,
+      accessStartDate,
+      accessExpiryDate,
     });
 
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    console.error('Error during registration:', error);
+    console.error("Error during registration:", error);
     res.status(500).json({ error: "Internal server error" });
   }
+};
+
+// login user
+export const loginUser = async (req, res) => {
+  
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerUser } from "../controllers/userController.js";
+import { loginUser, registerUser } from "../controllers/userController.js";
 
 const router = express.Router()
 
-router.post("/user", registerUser)
+router.post("/user/register", registerUser)
+router.post("/user/login", loginUser)
 
 export default router;
